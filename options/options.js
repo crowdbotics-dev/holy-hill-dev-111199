76d17f0a8a48ac1aa950b75393b30b19844872ac
Crowdbotics <engineering@crowdbotics.com>
2023-08-24 15:57:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-hill-dev-111199",
+  url: "https://holy-hill-dev-111199.botics.co",
+  api: "https://holy-hill-dev-111199.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
